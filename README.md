99f60807ca2b518d3a8a2f74652d99c666f8dbc5
haochengxia <xhc_1007@163.com>
2022-06-01 13:14:08 +0800

    p84

---
+++ README.md
@@ -0,0 +1,7 @@
+# Cpp Snippets
+
+Basic knowledge on Cpp language.
+
+## Books
+
+- C++ Templates (2nd) [[Read]](https://github.com/Walton1128/CPP-Templates-2nd--)  [[Snippets]](./cpp-templates)
--- cpp-templates/ch06.cpp
+++ cpp-templates/ch06.cpp
@@ -15,3 +15,23 @@ void print_typeof(T const& x)
 {
     std::cout << typeid(x).name() << std::endl;
 }
+
+// 手工实例化模板
+template void print_typeof<double>(double const&);
+
+// stack.hpp 声明
+// stackdef.hpp 定义
+// stackinst.cpp 显式实例化
+
+// 另一种组织方式为分类模型，利用export实现
+//export
+//template <typename T>
+//void print_typeof(T const&);
+
+// 如下方定义在不同文件，也会被隐式地export
+//template <typename T>
+//void print_typeof(T const& x)
+//{
+//    std::cout << typeid(x).name() << std::endl;
+//}
+
