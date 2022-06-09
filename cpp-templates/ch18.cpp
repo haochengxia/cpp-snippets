07b18d350b50c16569d409adf0d0a793a00f4fbf
haochengxia <xhc_1007@163.com>
2022-06-09 15:06:49 +0800

    p323

--- cpp-templates/ch18.cpp
+++ cpp-templates/ch18.cpp
@@ -4,3 +4,82 @@
 
 // 为了支持一种数值数组的类而引入 expression template
 
+// 表达式模板适用于能够在运行期确定大小、中等大小的数组
+
+// 在常规编程中，为了避免临时对象创建的开销，常用包含计算的赋值运算符来代替纯粹的赋值运算符
+
+// 在模板实参中编码表达式
+
+#include "stddef.h"
+#include "cassert"
+
+template <typename T> class A_Scalar;
+
+// primary template
+template <typename T>
+class A_Traits {
+public:
+    typedef T const& ExprRef;     // type to refer to is constant reference
+};
+
+template <typename T>
+class A_Scalar {
+private:
+    T const& s;
+public:
+    A_Scalar(T const& v) : s(v) {}
+    T operator[] (size_t) const {
+        return s;
+    }
+    size_t size() const {
+        return 0;
+    }
+};
+
+// partial specialization for scalars
+template <typename T>
+class A_Traits<A_Scalar<T> > {
+public:
+    typedef A_Scalar<T> ExprRef;  // type to refer to is ordinary value
+};
+
+template<typename T, typename OP1, typename OP2>
+class A_Add {
+private:
+    typename A_Traits<OP1>::ExprRef op1;
+    typename A_Traits<OP2>::ExprRef op2;
+public:
+    A_Add (OP1 const& a, OP2 const& b) : op1(a), op2(b) {
+
+    }
+
+    T operator[] (size_t idx) const {
+        return op1[idx] + op2[idx]
+    }
+
+    size_t size() const {
+        assert(op1.size() == 0 || op2.size()==0 || op1.size() == op2.size());
+        return op1.size()!=0? op1.size() : op2.size();
+    }
+};
+
+template <typename T, typename OP1, typename OP2>
+class A_Multi {
+private:
+    typename A_Traits<OP1>::ExprRef op1;
+    typename A_Traits<OP2>::ExprRef op2;
+
+public:
+    A_Add (OP1 const& a, OP2 const& b) : op1(a), op2(b) {
+
+    }
+
+    T operator[] (size_t idx) const {
+        return op1[idx] * op2[idx]
+    }
+
+    size_t size() const {
+        assert(op1.size() == 0 || op2.size()==0 || op1.size() == op2.size());
+        return op1.size()!=0? op1.size() : op2.size();
+    }
+};
---
+++ cpp-templates/ch19.cpp
@@ -0,0 +1,4 @@
+// 智能指针
+// Created by Percy on 2022/6/9.
+//
+
